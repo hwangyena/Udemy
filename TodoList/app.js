74490c95b694e01df84438f86c34ec070a2c99f3
hwangyena <center7332@naver.com>
2021-07-12 09:18:03 +0900

    add heroku

--- TodoList/app.js
+++ TodoList/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 ////////////////// mongodb //////////////////
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-yena:63535455@cluster0.a78gw.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 //schema
 const itemsSchema = {
