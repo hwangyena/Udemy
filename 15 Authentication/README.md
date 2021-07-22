6e4ef946e8684da84d355f3d08679eced746c09b
hwangyena <center7332@naver.com>
2021-07-22 22:22:49 +0900

    cookie and session

--- 15 Authentication/README.md
+++ 15 Authentication/README.md
@@ -1,8 +1,12 @@
 # Authentication
 
 ### 💁 학습한 내용
-* **Level 1** email, password DB 정보 비교     
-* **Level 2** Database Encryption    
-* **dotenv** .env 파일과 .gitignore 사용
-* **Level 3** Hash 함수
-* **Level 4** Salting
+1. Authentication Level별 분류
+> **Level 1** email, password DB 정보 비교     
+> **Level 2** Database Encryption    
+> **Level 3** Hash 함수
+> **Level 4** Salting
+
+
+2. **dotenv** .env 파일과 .gitignore 사용
+3. Cookies and Session 
--- 15 Authentication/app.js
+++ 15 Authentication/app.js
@@ -3,9 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
 
+const session = require('express-session')
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -17,21 +18,39 @@ console.log(process.env.API_KEY);
 console.log("strong hash: "+md5("12345abcdef"));
 */
 
+/*use*/
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true});
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*mongoose server connection*/
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
+
+/*Mongoose*/
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //////////////////////////////get///////////////////////////////
 app.get("/", (req, res)=>{
@@ -46,45 +65,53 @@ app.get("/register", (req, res)=>{
   res.render("register");
 });
 
+app.get("/secrets", (req, res)=>{
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }else{
+    res.redirect("/login");
+  }
+});
+
+/*session 종료*/
+app.get("/logout", (req, res)=>{
+  req.logout();
+  res.redirect("/");
+});
+
 //////////////////////////////post///////////////////////////////
 /*사용자 email, password 등록*/
 app.post("/register", (req, res)=>{
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-      const newUser = new User({
-        email: req.body.username,
-        password: hash
+  User.register({username: req.body.username}, req.body.password, (err, user)=>{
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }
+    else{
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
       });
-
-      newUser.save((err)=>{
-        if(err){
-          console.log(err);
-        }else{
-          res.render("secrets");
-        }
-      });
-    });
+    }
+  });
 });
 
 /*사용자 email, password 확인*/
 app.post("/login", (req, res)=>{
-  const username = req.body.username;
-  const password = req.body.password; //hash한 결과와 비교
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
-  User.findOne({email: username}, (err, foundUser)=>{
+  req.login(user, (err)=>{
     if(err){
       console.log(err);
     }else{
-      if(foundUser){ //user 존재 -> password 확인
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-            if(result === true){
-              res.render("secrets");
-            }
-        });
-      }
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
     }
   });
-})
-
+});
 
 
 app.listen(3000, ()=>{
