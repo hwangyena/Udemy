18f6cd2b1e5cfd6dfc091ffeaf9191df84dcd9c2
hwangyena <center7332@naver.com>
2021-08-31 16:03:12 +0900

    Component Tree - InputArea

--- 16 React/src/components/App.jsx
+++ 16 React/src/components/App.jsx
@@ -1,27 +1,21 @@
 import React, { useState } from "react";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-  const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
-
-  function addItem() {
+  function addItem(text) {
     setItems((prevItems) => {
-      return [...prevItems, inputText];
+      return [...prevItems, text];
     });
-    setInputText("");
   }
 
-  function deleteItem(id){
-    setItems((prevItems)=>{
-      return (prevItems.filter((element, idx)=>{
-        return idx != id;
-      }))
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((element, idx) => {
+        return idx !== id;
+      });
     });
   }
 
@@ -30,21 +24,16 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea addEvent={addItem} />
       <div>
         <ul>
           {items.map((todoItem, index) => (
             <ToDoItem
-              id={index}
               key={index}
+              id={index}
               text={todoItem}
               onChecked={deleteItem}
-              />
+            />
           ))}
         </ul>
       </div>
---
+++ 16 React/src/components/InputArea.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function InputArea(props) {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const text = event.target.value;
+    setInputText(text);
+  }
+
+  return (
+    <div className="form">
+      <input onChange={handleChange} type="text" value={inputText} />
+      <button
+        onClick={() => {
+          props.addEvent(inputText);
+          setInputText("");
+        }}
+      >
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+
+export default InputArea;
